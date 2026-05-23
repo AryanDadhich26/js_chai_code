63821d8ab20c26ed2cc7bd375e1b65e61a3703dc
AryanDadhich26 <aryandadhichksg@gmail.com>
2026-05-23 13:43:58 +0000

    conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33"
+console.log(typeof score)
+
+//converting string to Number
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+// but if score is "33abc" converion will not show error but valueInNumbers will be Nan if score is null valueInNumber converts to 0
+//33=>33
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 ; false=>0
+//null=>0
+
+
+// for Boolean converter
+// 1=>true; 0=>false
+// " "=>fasle; "Aryan"=>true
+
