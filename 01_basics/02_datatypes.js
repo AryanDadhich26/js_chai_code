d47b45fff6dcc09fcb470f76131eb1f4b08e276b
AryanDadhich26 <aryandadhichksg@gmail.com>
2026-05-23 13:43:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"//treat all JS code as newer version
+// alert(3+3) -> will gave error bcz we are using nodejs not browser 
+let name = "Aryan"
+alert("hello")
+console.log(typeof "aryan")
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
