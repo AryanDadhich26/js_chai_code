a02c8446f634da8f8f153136d987990a1326c247
AryanDadhich26 <aryandadhichksg@gmail.com>
2026-05-23 18:04:55 +0000

    maths and num

---
+++ 01_basics/05_nums_math.js
@@ -0,0 +1,8 @@
+// specially defining it is number:
+const score = new Number(100)
+console.log(score)
+console.log(score.toString())
+console.log(score.toFixed(2))//defines that it will have only 2 decimal values
+score.toPrecision(4)//kitni values p dhyaan dena h before point
+//Math is a library in it:
+//Math.abs, Math.round, Math.ceil, Math.floor, Math.min, Math.max, (Math.random()*(max-min+1))+min=>for random value between the range min and max
