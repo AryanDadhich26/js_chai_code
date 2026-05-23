59bfb5fa9a7d20dd2c99c8ec2a5cb0228b5e574c
AryanDadhich26 <aryandadhichksg@gmail.com>
2026-05-23 17:04:20 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+//two types of data type on the basis of how they are stored in memory:
+// 1.Primitive data type:(stored directly by value,immutable, stored in stack, holds only single value)
+    //7 types: String, Number, Boolean, null, undefined, Symbol
+//2.Refrence(Non Primitive):(stored by refrence, mutable, stored in heap, can hold collections of value)
+    //Array, Objects, Functions
+//Array=> cont heros=["asbd","adjbcskd"]
+//Object=> let myObj= {
+            // name:"hitesh",
+            // age:22
+// }
+
+//function: const func= function(){}
